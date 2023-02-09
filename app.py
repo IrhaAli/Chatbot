bf84d8c7210657b948c4e816113b6433b78ff002
Irha Ali <irhaali_97@hotmail.ca>
2023-02-09 12:15:55 -0500

    Finish finish (kinda)

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import requests
 
 app = Flask(__name__)
 
-# Route to the chat page
-
 
 @app.route("/")
 def index():
@@ -13,28 +11,28 @@ def index():
 
 @app.route("/get")
 def get_bot_response():
-    user_message = requests.args.get('msg')
-    api_key = "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"
-    model = "text-davinci-003"
+    user_message = request.args.get('msg')
+    api_key = "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"
+    model = "text-davinci-002"
     response = generate_response_gpt3(user_message, model, api_key)
     return response
 
 
-def generate_response_gpt3(user_messages, model, api_key):
-    prompt = (f"User: {user_messages}\n"
+def generate_response_gpt3(user_message, model, api_key):
+    prompt = (f"User: {user_message}\n"
               f"ChatBot:")
     response = requests.post(
-        "https://api.openapi.com/v1/engines/text-davinci-003/completions",
+        "https://api.openai.com/v1/engines/text-davinci-002/completions",
         headers={"Authorization": f"Bearer {api_key}"},
         json={
             "prompt": prompt,
-            "max_tokens": 200,
+            "max_tokens": 100,
             "temperature": 0.7,
-        }
+        },
     ).json()
-    return response('choices')[0]["text"].strip()[len(prompt):]
+
+    return response['choices'][0]['text'].strip()[len(prompt):]
 
 
-# Run the app
 if __name__ == "__main__":
     app.run()
