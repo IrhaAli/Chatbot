72be293224999b99d06d0de93ac8f12b0d3a3f5a
Irha Ali <irhaali_97@hotmail.ca>
2023-02-09 11:52:52 -0500

    Chatbot in progress

--- app.py
+++ app.py
@@ -1,38 +1,9 @@
-import nltk
-from nltk.chat.util import Chat, reflections
 from flask import Flask, request, render_template
+import requests
 
 app = Flask(__name__)
 
-pairs = [
-    [
-        r"My name is (.*)",
-        ["Hello %1, how are you today?, hey %1 %1 how's GHW going?"]
-    ],
-    [
-        r"What is your name?",
-        ["My name is gene", "I'm the cutest GHW gene bot",
-         "It's nice to meet you I'm gene"]
-    ],
-    [
-        r"sorry (.*)",
-        ["It's alright", "Never gonna give you up"]
-    ],
-    [
-        r"Hey, I'm rick",
-        ["Go away", "I will give you up"]
-    ],
-    [
-        r"Quit",
-        ["Bye it was great talking to you", "See you soon"]
-    ],
-    [
-        r"What categories are there in GHW?",
-        ["There are many categories and they can be found here"]
-    ],
-]
-
-chatbot = Chat(pairs, reflections)
+# Route to the chat page
 
 
 @app.route("/")
@@ -40,12 +11,30 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/chat", methods=["POST"])
-def chat():
-    message = request.form["text"]
-    response = chatbot.respond[message]
+@app.route("/get")
+def get_bot_response():
+    user_message = requests.args.get('msg')
+    api_key = "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"
+    model = "text-davinci-003"
+    response = generate_response_gpt3(user_message, model, api_key)
     return response
 
 
+def generate_response_gpt3(user_messages, model, api_key):
+    prompt = (f"User: {user_messages}\n"
+              f"ChatBot:")
+    response = requests.post(
+        "https://api.openapi.com/v1/engines/text-davinci-003/completions",
+        headers={"Authorization": f"Bearer {api_key}"},
+        json={
+            "prompt": prompt,
+            "max_tokens": 200,
+            "temperature": 0.7,
+        }
+    ).json()
+    return response('choices')[0]["text"].strip()[len(prompt):]
+
+
+# Run the app
 if __name__ == "__main__":
     app.run()
